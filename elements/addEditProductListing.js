4a83b59ec313be70f1420ee6987c65058bcdc4b5
cjp-engr <engr.carmenjoy14@gmail.com>
2026-08-25 00:00:08 +0800

    add product simple flow

---
+++ elements/addEditProductListing.js
@@ -0,0 +1,15 @@
+output.addEditProductListing = {
+    shipping: {
+        standardButton: 'wizard_shipping_option_standard',
+        expressButton: 'wizard_shipping_option_express',
+        pickupButton: 'wizard_shipping_option_pickup',
+
+        freeShippingButton: 'wizard_free_shipping_button',
+        buyerPaysButton: 'wizard_buyer_pays_button',
+
+        standardFeeField: 'wizard_shipping_fee_field_standard',
+        expressFeeField: 'wizard_shipping_fee_field_express',
+        pickupFeeField: 'wizard_shipping_fee_field_pickup',
+    }
+
+}
