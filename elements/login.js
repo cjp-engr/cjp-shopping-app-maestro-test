90168f314509c017af43a4bc45db7d6d22240061
cjp-engr <engr.carmenjoy14@gmail.com>
2026-08-24 19:07:40 +0800

    add test cases files

--- elements/login.js
+++ elements/login.js
@@ -2,5 +2,5 @@
 output.login = {
     loginEmailField: 'login_email_field',
     loginPasswordField: 'login_password_field',
-    loginButton: 'login_button'
+    loginButton: 'login_button' //if this is semantics - identifier
 }
