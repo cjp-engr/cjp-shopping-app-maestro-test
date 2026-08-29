5c0214b1127da7819d692a1c128843d2a04f5f1f
cjp-engr <engr.carmenjoy14@gmail.com>
2026-08-29 15:42:51 +0800

    add product variant flow 1

--- elements/selectors/addEditProductListing.js
+++ elements/selectors/addEditProductListing.js
@@ -6,6 +6,10 @@ output.addEditProductListing = {
         attributeAddValueField: 'wizard_attr_add_value_field',
         attributeAddValueButton: 'wizard_attr_add_value_button',
         attributeConfirmButton: 'wizard_attr_confirm_button',
+        variantPriceField: 'wizard_variant_price_field_',
+        variantStockField: 'wizard_variant_stock_field_',
+        variantDiscountField: 'wizard_variant_discount_field_',
+        variantSkuField: 'wizard_variant_sku_field_',
     },
     shipping: {
         standardButton: 'wizard_shipping_option_standard',
