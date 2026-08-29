b12fb4ba33eceab327274227fbaa0f5f4fd38c46
cjp-engr <engr.carmenjoy14@gmail.com>
2026-08-29 22:44:49 +0800

    edit product simple flow 1

--- elements/selectors/sellerDashboard.js
+++ elements/selectors/sellerDashboard.js
@@ -2,4 +2,11 @@ output.sellerDashboard = {
     addProductFab: 'add_product_fab',
     // prefix: append productId → e.g. edit_product_button_<id>
     editProductButton: 'edit_product_button_',
+
+    list: {
+        category: 'product_tile_category_',
+        productName: 'product_tile_name_',
+        price: 'product_tile_price_',
+        stock: 'product_tile_stock_'
+    }
 }
