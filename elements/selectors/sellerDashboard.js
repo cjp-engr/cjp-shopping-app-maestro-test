08294884e03e8af3f355e0bfce5a1ffbb8046fca
cjp-engr <engr.carmenjoy14@gmail.com>
2026-09-03 23:00:12 +0800

    feat: delete product simple flow

--- elements/selectors/sellerDashboard.js
+++ elements/selectors/sellerDashboard.js
@@ -1,13 +1,18 @@
 output.sellerDashboard = {
     addProductFab: 'add_product_fab',
-    // prefix: append productId → e.g. edit_product_button_<id>
+    // prefix: append productId → e.g. edit_product_button_<productName>
     editProductButton: 'edit_product_button_',
+    // prefix: append productId → e.g. delete_product_button_<productName>
+    deleteProductButton: 'delete_product_button_',
 
     list: {
         // prefix: append productName → e.g. product_tile_xxx_<productName>
-        category: 'product_tile_category_', 
+        category: 'product_tile_category_',
         productName: 'product_tile_name_',
         price: 'product_tile_price_',
         stock: 'product_tile_stock_'
+    },
+    deleteDialog: {
+        dialogTitle: 'dialog_title_Delete Product'
     }
 }
